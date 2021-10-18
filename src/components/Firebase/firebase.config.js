ea5f3bb031c637193302fef49f6f15ca23a2fe9a
asmaliha <afia.maliha65@gmail.com>
2021-10-18 09:00:06 +0600

    first commit

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBn-ywFwDE84Uo1QCCHZBFAPj0zGDgsvcg",
+    authDomain: "react-auth-pr.firebaseapp.com",
+    projectId: "react-auth-pr",
+    storageBucket: "react-auth-pr.appspot.com",
+    messagingSenderId: "774129837056",
+    appId: "1:774129837056:web:ce8b4aaa55ec9994b064e5"
+};
+
+export default firebaseConfig;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useFirebase from '../../hook/useFIrebase';
+import './Login.css'
+
+const Login = () => {
+    const { signInUsingGoogle } = useFirebase();
+    return (
+        <div>
+            <h2>Please Login</h2>
+            <button onClick={signInUsingGoogle}>Google Sign in</button>
+            <br />
+            <Link to='/register'>New User?</Link>
+        </div>
+    );
+};
+export default Login;
